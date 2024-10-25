71aff56fdea9b986aff29bc0df5547efbd0d5252
Athulyasuresh1 <athullyal2000@gmail.com>
2024-10-25 10:30:34 +0530

    Exception handling

---
+++ samplemaven/src/main/java/exceptionhandling/ExceptionNew.java
@@ -0,0 +1,11 @@
+package exceptionhandling;
+
+public class ExceptionNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String s=null;
+      System.out.println(s.length());
+	}
+
+}
